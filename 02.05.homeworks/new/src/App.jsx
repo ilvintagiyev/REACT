dc76ddf118534605280a0ed68aba8c3a0bd8fa59
“ilvintagiyev” <ilvin.tagiyev88@gmail.com>
2024-05-02 11:34:39 +0400

    push

---
+++ 02.05.homeworks/new/src/App.jsx
@@ -0,0 +1,42 @@
+
+import './App.css'
+import PrimarySearchAppBar from './components/Header'
+import SkeletonComp from './components/Skeleton'
+import MovieCard from './components/Card'
+import { useEffect, useState } from 'react'
+import { getAll } from './requests/request.js'
+
+
+function App() {
+  const arr = Array.from({ length: 6 })
+  const [products, setProducts] = useState([])
+  const [skeleton, setSkeleton] = useState(true);
+  useEffect(() => {
+    getAll('/products')
+      .then((data) => {
+        setProducts(data);
+        setSkeleton(false);
+      })
+      .catch((error) => console.log(error));
+
+  }, []);
+
+  console.log(products);
+  return (
+    <>
+      <PrimarySearchAppBar />
+      <div className='container'>
+        <div className='row'>
+          {arr && arr.map((s, i) => {
+            return <SkeletonComp />
+          })}
+        </div>
+      </div>
+      
+
+
+    </>
+  )
+}
+
+export default App
